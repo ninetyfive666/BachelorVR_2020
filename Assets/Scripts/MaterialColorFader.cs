67960fccac5defd0501bbaae5105ecc68b1eab75
ninetyfive666 <40063412+ninetyfive666@users.noreply.github.com>
2020-12-28 20:25:39 +0100

    Migration of BachelorVR
    - Material Migration
    - Texture Migration
    - Script Migration
    - PostPorcess Migration

--- Assets/Scripts/MaterialColorFader.cs
+++ Assets/Scripts/MaterialColorFader.cs
@@ -12,24 +12,24 @@ public class MaterialColorFader : MonoBehaviour
     public void EnableHighlight()
     {
           var rend = gameObject.GetComponent<Renderer>();
-          rend.material.SetColor("_EmissiveColor", Highlight);
+          rend.material.SetColor("_EmissionColor", Highlight);
     }
 
     public void DisableHighlight()
     {
         var rend = gameObject.GetComponent<Renderer>();
-        rend.material.SetColor("_EmissiveColor", Color.white);
+        rend.material.SetColor("_EmissionColor", Color.white);
     }
 
     public void EnableOutline()
     {
         var rend = gameObject.GetComponent<Renderer>();
-        rend.material.SetColor("_EmissiveColor", Outline);
+        rend.material.SetColor("_EmissionColor", Outline);
     }
 
     public void DisableOutline()
     {
         var rend = gameObject.GetComponent<Renderer>();
-        rend.material.SetColor("_EmissiveColor", Transparent);
+        rend.material.SetColor("_EmissionColor", Transparent);
     }
 }
