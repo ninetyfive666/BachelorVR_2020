0de97fdd0f8beb2f63913acce857f8c7c8a33c0a
ninetyfive666 <40063412+ninetyfive666@users.noreply.github.com>
2021-01-04 19:31:36 +0100

    Linked Audio Player and Quest Script
    - AudioPlayer Script is now the main driver of all Voiceover
    - Reworked Quest script and added more interactions
    - Added Wood

--- Assets/Scripts/ObjectAudioPlayer.cs
+++ Assets/Scripts/ObjectAudioPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectAudioPlayer : MonoBehaviour
 {   
-    [Tooltip("Bilder")]
+    [Header("Bilder")]
     public AudioClip Opa;
     public AudioClip Testament;
     public AudioClip Holz;
@@ -13,10 +13,27 @@ public class ObjectAudioPlayer : MonoBehaviour
     public AudioClip Naabquelle;
     public AudioClip Wiesenfest;
 
-    [Tooltip("Objekte")]
+    [Header("Objekte")]
     public AudioClip Sofa;
 
+    [Header("PlayOnlyOnce")]
+    public AudioClip Kartoffel;
+    public AudioClip Ei;
+    public AudioClip Karotte;
+    public AudioClip Feuerholz;
+
+    [Header("Quest Steps")]
+    public AudioClip questStep1;
+    public AudioClip questStep2;
+    public AudioClip questStep3;
+
     AudioSource audioSource;
+
+    bool bEi = false;
+    bool bKartoffel = false;
+    bool bFeuerholz = false;
+    bool bKarotte = false;
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -28,46 +45,91 @@ public class ObjectAudioPlayer : MonoBehaviour
         audioSource.clip = (Opa);
         audioSource.Play();
     }
-
     public void PlayTestament()
     {
         audioSource.clip = (Testament);
         audioSource.Play();
     }
-
     public void PlayHolz()
     {
         audioSource.clip = (Holz);
         audioSource.Play();
     }
-
     public void PlayDreher()
     {
         audioSource.clip = (Dreher);
         audioSource.Play();
     }
-
     public void PlayHandball()
     {
         audioSource.clip = (Handball);
         audioSource.Play();
     }
-
     public void PlayNaabquelle()
     {
         audioSource.clip = (Naabquelle);
         audioSource.Play();
     }
-
     public void PlayWiesenfest()
     {
         audioSource.clip = (Wiesenfest);
         audioSource.Play();
     }
-
     public void PlaySofa()
     {
         audioSource.clip = (Sofa);
+        audioSource.Play();
+    }
+    public void PlayFeuerholz()
+    {
+        if (bFeuerholz == false)
+        {
+            audioSource.clip = (Feuerholz);
+            audioSource.Play();
+            bFeuerholz = true;
+        }
+    }
+    public void PlayKartoffel()
+    {   
+        if (bKartoffel == false)
+        {
+            audioSource.clip = (Feuerholz);
+            audioSource.Play();
+            bKartoffel = true; 
+        }
+
+    }
+    public void PlayEi()
+    {
+        if (bEi == false)
+        {
+            audioSource.clip = (Feuerholz);
+            audioSource.Play();
+            bEi = true;
+        }
+
+    }
+
+    public void PlayKarotte()
+    {
+        if (bKarotte == false)
+        {
+            audioSource.clip = (Karotte);
+            audioSource.Play();
+            bKarotte = true;
+        }
+
+    }
+
+    public void PlayQuestStep1()
+    {
+        audioSource.clip = (questStep1);
+        audioSource.Play();
+    }
+
+    public void PlayQuestStep2()
+    {
+        audioSource.clip = (questStep2);
         audioSource.Play();
     }
 }
--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Quest : MonoBehaviour
 {
+    [Header("AudioPlayer")]
+    public ObjectAudioPlayer objectAudioPlayer;
 
+    [Header("Collectables")]
     public GameObject Kartoffel;
     public GameObject Ei;
+    public GameObject Karotte;
+
+    [Header("Topf")]
     public GameObject Prop;
+
+    [Header("Leitsystem")]
+    public LineRenderer Line1;
+    public LineRenderer Line2;
+    public LineRenderer Line3;
+    public LineRenderer Line4;
 
     Rigidbody rbKartoffel;
     Rigidbody rbEi;
+    Rigidbody rbKarotte;
 
     Transform parentTransform;
     MeshCollider eimerCollider;
@@ -18,7 +31,10 @@ public class Quest : MonoBehaviour
 
     bool bEi = false;
     bool bKartoffel = false;
-    bool bAudio = false;
+    bool bKarotte = false;
+
+    bool questStep1Complete = false;
+    bool questStep2Complete = false;
 
 
     void Start()
@@ -29,14 +45,21 @@ public class Quest : MonoBehaviour
 
         rbKartoffel = Kartoffel.GetComponent<Rigidbody>();
         rbEi = Ei.GetComponent<Rigidbody>();
+        rbKarotte = Karotte.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (bEi == true && bKartoffel == true && bAudio == false)
+        if (bEi == true && bKartoffel == true && questStep1Complete == false)
         {
-            AudioPlayer();
-            bAudio = true;
+            QuestStep1();
+            questStep1Complete = true;
+        }
+
+        if (bKarotte == true & questStep2Complete == false)
+        {
+            QuestStep2();
+            questStep2Complete = true;
         }
 
     }
@@ -57,11 +80,28 @@ public class Quest : MonoBehaviour
             Ei.transform.SetParent(parentTransform);
             rbEi.isKinematic = true;
         }
+
+        if (collision.transform.gameObject == Karotte)
+        {
+            bKarotte = true;
+            Karotte.transform.SetParent(parentTransform);
+            rbKarotte.isKinematic = true;
+        }
     }
 
-    void AudioPlayer()
+    void QuestStep1()
     {
-        audioSource.Play();
+        objectAudioPlayer.PlayQuestStep1();
+        Line1.enabled = false;
+        Line2.enabled = false;
+        Line3.enabled = true;
+    }
+
+    void QuestStep2()
+    {
+        objectAudioPlayer.PlayQuestStep2();
+        Line3.enabled = false;
+        Line4.enabled = true;
         Prop.SetActive(true);
     }
 
@@ -74,7 +114,6 @@ public class Quest : MonoBehaviour
 
         rbEi.isKinematic = false;
         rbKartoffel.isKinematic = false;
-        
-        
+        rbKarotte.isKinematic = false;
     }
 }
