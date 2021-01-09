5370b5da7d18e36790cad8e8b3f7286a67666422
ninetyfive666 <40063412+ninetyfive666@users.noreply.github.com>
2021-01-09 13:28:48 +0100

    Added Surge and Janky Teleportation Vigniette Functionality

--- Assets/Scripts/RoomDestinationTeleport.cs
+++ Assets/Scripts/RoomDestinationTeleport.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using Pixelplacement;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class RoomDestinationTeleport : MonoBehaviour
 {
@@ -11,29 +14,70 @@ public class RoomDestinationTeleport : MonoBehaviour
     public GameObject DestinationKüche;
     public GameObject DestinationGarten;
 
+
+    public float intensity = 0.75f;
+    public float duration = 0.5f;
+    public Volume volume = null;
+
+    private Vignette vignette = null;
+
+
+
+    public void FadeIn()
+    {
+        Tween.Value(0, intensity, ApplyValue, duration, 0, Tween.EaseOut);
+    }
+
+    public void FadeOut()
+    {
+        Tween.Value(intensity, 0, ApplyValue, duration, duration, Tween.EaseOut);
+    }
+
+
+    public void Awake()
+    {
+        if (volume.profile.TryGet(out Vignette vignette))
+            this.vignette = vignette;
+    }
+
     public void TeleportKeller()
     {
-        XRRig.transform.position = DestinationKeller.transform.position;
+        FadeIn();
+        Tween.Position(XRRig.transform, XRRig.transform.position, DestinationKeller.transform.position, 0.01f, duration);
+        FadeOut();
     }
 
     public void TeleportWaschküche()
     {
-        XRRig.transform.position = DestinationWaschküche.transform.position;
+        FadeIn();
+        Tween.Position(XRRig.transform, XRRig.transform.position, DestinationWaschküche.transform.position, 0.01f, duration);
+        FadeOut();
     }
 
     public void TeleportKüche()
     {
-        XRRig.transform.position = DestinationKüche.transform.position;
+        FadeIn();
+        Tween.Position(XRRig.transform, XRRig.transform.position, DestinationKüche.transform.position, 0.01f, duration);
+        FadeOut();
     }
 
     public void TeleportGarten()
     {
-        XRRig.transform.position = DestinationGarten.transform.position;
+        FadeIn();
+        Tween.Position(XRRig.transform, XRRig.transform.position, DestinationGarten.transform.position, 0.01f, duration);
+        FadeOut();
     }
 
     public void TeleportSchlafzimmer()
     {
-        XRRig.transform.position = DestinationSchlafzimmer.transform.position;
+        FadeIn();
+        Tween.Position(XRRig.transform, XRRig.transform.position, DestinationSchlafzimmer.transform.position, 0.01f, duration);
+        FadeOut();
+    }
+
+    private void ApplyValue(float value)
+    {
+        vignette.intensity.Override(value);
     }
 
 }
