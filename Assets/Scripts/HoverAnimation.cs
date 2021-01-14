a77fdc60c08e602a4de7894a8b40565248e0b1fa
ninetyfive666 <40063412+ninetyfive666@users.noreply.github.com>
2021-01-14 15:38:04 +0100

    Extended UI Hover Script with Door Opening Functionality

--- Assets/Scripts/HoverAnimation.cs
+++ Assets/Scripts/HoverAnimation.cs
@@ -11,6 +11,13 @@ public class HoverAnimation : MonoBehaviour
     [Range(0f, .5f)]
     public float offset = .1f;
 
+    public Vector3 rotationGoal = new Vector3(0,-10, 0);
+    Vector3 rotationOrigin;
+
+    public GameObject door;
+
+
+
     public Color32 white = new Color32(255, 255, 255, 255);
     public Color32 transparent = new Color32(255, 255, 255, 0);
 
@@ -19,31 +26,38 @@ public class HoverAnimation : MonoBehaviour
 
     Vector3 originPosition;
     Vector3 hoverPosition;
+
     void Start()
     {
         rend = GetComponent<MeshRenderer>();
         rend.material.SetColor("_BaseColor", transparent);
+
+        if(door != null)
+        {
+            rotationOrigin = door.transform.localRotation.eulerAngles;
+        }
 
         origin = GetComponent<Transform>();
         originPosition = origin.transform.position;
         hoverPosition = origin.transform.position + Vector3.down * offset;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void HoverEnter()
     {
         Tween.Position(origin, origin.position, hoverPosition, duration, 0, Tween.EaseOut);
         Tween.ShaderColor(rend.material, ("_BaseColor"), transparent, white, duration, 0, Tween.EaseOut);
+        if(door != null) { 
+        Tween.LocalRotation(door.transform, rotationOrigin, rotationGoal, duration*2, 0, Tween.EaseOut);
+        }
     }
 
     public void HoverExit()
     {
         Tween.Position(origin, origin.position, originPosition, duration, 0, Tween.EaseOut);
         Tween.ShaderColor(rend.material, ("_BaseColor"), white, transparent, duration, 0, Tween.EaseOut);
+        if (door != null)
+        {
+            Tween.LocalRotation(door.transform, rotationGoal, rotationOrigin, duration, 0, Tween.EaseOut);
+        }
     }
 }
